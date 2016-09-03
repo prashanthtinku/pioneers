0df17967b807080c7470aafc5840594f6773dc4f
prashanth <prashanth.qatesting@gmail.com>
2016-09-02 23:58:18 -0700

    Main Open browser

---
+++ QA/SMOKE/src/levelOnePackage/Navigation.java
@@ -0,0 +1,14 @@
+package levelOnePackage;
+
+public class Navigation {
+
+	public static void main(String[] args) {
+		OpenBrowser();
+
+	}
+	
+	public static void OpenBrowser(){
+		System.out.println("Open Browser");
+	}
+
+}
