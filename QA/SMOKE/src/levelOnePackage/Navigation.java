d2b020b1d2e6251e72791556fc7cb7eccda26e72
prashanth <prashanth.qatesting@gmail.com>
2016-09-02 23:59:04 -0700

    Enter URL

--- QA/SMOKE/src/levelOnePackage/Navigation.java
+++ QA/SMOKE/src/levelOnePackage/Navigation.java
@@ -4,11 +4,16 @@ public class Navigation {
 
 	public static void main(String[] args) {
 		OpenBrowser();
+		EnterURL();
 
 	}
 	
 	public static void OpenBrowser(){
 		System.out.println("Open Browser");
+	}
+	
+	public static void EnterURL(){
+		System.out.println("Open URL");
 	}
 
 }
